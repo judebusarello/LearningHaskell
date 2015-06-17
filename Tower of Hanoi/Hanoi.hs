c4c38d7ba126199df92f274ecf228bc9a0ead373
Jude Busarello <BUSJU01@BUSJU01W7.CA.COM>
2015-06-17 11:10:35 -0400

    Fixed error with parsing and took out unneeded pattern. Looks like it works!

--- Tower of Hanoi/Hanoi.hs
+++ Tower of Hanoi/Hanoi.hs
@@ -2,12 +2,8 @@ type Peg = String
 type Move = (Peg, Peg)
 
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
-hanoi n start temp end 
-  hanoi 0 _ _ _ = []
-  hanoi 1 start _ end = [(start, end)]
-  hanoi n start temp end =
-    hanoi (n - 1) start end temp
-    hanoi n start temp end
-    hanoi (n - 1) temp start end
-
-
+hanoi 0 _ _ _ = []
+hanoi n start temp end =
+     hanoi (n - 1) start end temp
+     ++ [(start,end)]
+     ++ hanoi (n - 1) temp start end
